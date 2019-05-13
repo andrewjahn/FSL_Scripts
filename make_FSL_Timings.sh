2eaaacbfa82a8c90f4006a5dc0f1698bf97f6b85
andrewjahn <48071253+andrewjahn@users.noreply.github.com>
2019-05-13 11:47:23 -0400

    Update make_FSL_Timings.sh

--- make_FSL_Timings.sh
+++ make_FSL_Timings.sh
@@ -7,7 +7,9 @@ fi
 
 #Loop over all subjects and format timing files into FSL format
 for subj in $(cat subjList.txt) ; do
-        cd $subj/func
+        cd $subj/func #Navigate to the subject's func directory, which contains the timing files
+        
+        #Extract the onset times for the incongruent and congruent trials for each run. NOTE: This script only extracts the trials in which the subject made a correct response. Accuracy is nearly 100% for all subjects, but as an exercise the student can modify this to extract the incorrect trials as well.
         cat ${subj}_task-flanker_run-1_events.tsv | awk '{if ($3=="incongruent_correct") {print $1, $2, "1"}}' > incongruent_run1.txt
         cat ${subj}_task-flanker_run-1_events.tsv | awk '{if ($3=="congruent_correct") {print $1, $2, "1"}}' > congruent_run1.txt
 
