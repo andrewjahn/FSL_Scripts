c991c2aafc19b993cca393fc3f36f96f0ec26f41
andrewjahn <48071253+andrewjahn@users.noreply.github.com>
2019-05-13 11:51:07 -0400

    Update run_1stLevel_Analysis.sh

--- run_1stLevel_Analysis.sh
+++ run_1stLevel_Analysis.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Generate the subject list.  Do it this way to make modifying this script
-# to run just a subset of subjects easier.  Otherwise we might end up with
-# a bunch of feat+ directories.
+# Generate the subject list to make modifying this script
+# to run just a subset of subjects easier.
 
-# Running from sub-02 onward
 for id in {01..26} ; do
     subj="sub-$id"
     echo "===> Starting processing of $subj"
@@ -14,7 +12,7 @@ for id in {01..26} ; do
         # If the brain mask doesn’t exist, create it
         if [ ! -f anat/${subj}_T1w_brain_f02.nii.gz ]; then
             bet2 anat/${subj}_T1w.nii.gz \
-                anat/${subj}_T1w_brain_f02.nii.gz -f 0.2
+                anat/${subj}_T1w_brain_f02.nii.gz -f 0.2 #Note: This fractional intensity appears to work well for most of the subjects in the Flanker dataset. You may want to change it if you modify this script for your own study.
         fi
 
         # Copy the design files into the subject directory, and then
@@ -24,14 +22,13 @@ for id in {01..26} ; do
 
         # Note that we are using the | character to delimit the patterns
         # instead of the usual / character because there are / characters
-        # in the pattern.  This is a handy trick; remember it; you may
-        # use it again; and again.
+        # in the pattern.
         sed -i "s|sub-08|${subj}|g" \
             design_run1.fsf
         sed -i "s|sub-08|${subj}|g" \
             design_run2.fsf
 
-        # Now everythin is set up to run feat
+        # Now everything is set up to run feat
         echo "===> Starting feat for run 1"
         feat design_run1.fsf
         echo "===> Starting feat for run 2"
