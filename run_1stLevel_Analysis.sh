ac3736286264c7a84c1d03f5e4e18116ad01409f
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2020-08-28 11:11:50 -0400

    Update run_1stLevel_Analysis.sh

--- run_1stLevel_Analysis.sh
+++ run_1stLevel_Analysis.sh
@@ -8,12 +8,14 @@ for id in `seq -w 1 26` ; do
     echo "===> Starting processing of $subj"
     echo
     cd $subj
-
+        
         # If the brain mask doesn’t exist, create it
         if [ ! -f anat/${subj}_T1w_brain_f02.nii.gz ]; then
+            echo "Skull-stripped brain not found, using bet with a fractional intensity threshold of 0.2"
+            # Note: This fractional intensity appears to work well for most of the subjects in the
+            # Flanker dataset. You may want to change it if you modify this script for your own study.
             bet2 anat/${subj}_T1w.nii.gz \
-                echo "Skull-stripped brain not found, using bet with a fractional intensity threshold of 0.2" \
-                anat/${subj}_T1w_brain_f02.nii.gz -f 0.2 #Note: This fractional intensity appears to work well for most of the subjects in the Flanker dataset. You may want to change it if you modify this script for your own study.
+                anat/${subj}_T1w_brain_f02.nii.gz -f 0.2
         fi
 
         # Copy the design files into the subject directory, and then
