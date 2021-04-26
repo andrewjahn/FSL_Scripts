7f3d93c3b9746416bbeacb265ab54a6e0c531c1d
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2021-04-26 13:11:14 -0400

    Update run_1stLevel_Analysis_Gambles.sh

--- run_1stLevel_Analysis_Gambles.sh
+++ run_1stLevel_Analysis_Gambles.sh
@@ -15,7 +15,7 @@ for id in `seq -w 1 16` ; do
             # Note: This fractional intensity appears to work well for most of the subjects in the
             # Flanker dataset. You may want to change it if you modify this script for your own study.
             bet2 anat/${subj}_T1w.nii.gz \
-                anat/${subj}_T1w_brain.nii.gz -f 0.2
+                anat/${subj}_T1w_brain.nii.gz
         fi
 
         # Copy the design files into the subject directory, and then
