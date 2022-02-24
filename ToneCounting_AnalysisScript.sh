cda9f9c401d11b267da71e7540f3665f52520d8b
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2022-02-24 15:48:12 -0500

    Update ToneCounting_AnalysisScript.sh

--- ToneCounting_AnalysisScript.sh
+++ ToneCounting_AnalysisScript.sh
@@ -3,7 +3,7 @@
   # Generate the subject list to make modifying this script
   # to run just a subset of subjects easier.
 
-  for id in `seq -w 1 2` ; do
+  for id in 01 ; do
       subj="sub-$id"
       echo "===> Starting processing of $subj"
       echo
